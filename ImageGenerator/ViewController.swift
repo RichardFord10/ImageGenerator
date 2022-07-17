6baaaa9401d55d0507c292405c19de4a0eeb8c1d
Richard Ford <36119878+RichardFord10@users.noreply.github.com>
2022-07-17 11:55:12 -0400

    Initial Commit

---
+++ ImageGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGenerator
+//
+//  Created by Macbook Pro on 7/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
